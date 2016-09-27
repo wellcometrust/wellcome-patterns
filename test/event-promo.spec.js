e9f2ab57841845b44b2ecf7f5ab4aad97d330d75
David McCormick <d.mccormick@wellcome.ac.uk>
2016-09-27 16:10:59 +0100

    Fix event promo test

--- test/event-promo.spec.js
+++ test/event-promo.spec.js
@@ -4,9 +4,10 @@ const { expect } = require('chai')
 const React = require('react')
 const EventPromo = require('../js/components/event-promo/index')
 const { shallow } = require('enzyme')
+const defaultData = require('../js/components/event-promo/data/default')
 
 describe('Component | <EventPromo />', () => {
   it('should render', () => {
-    expect(shallow(<EventPromo />).is('.event-promo')).to.equal(true);
+    expect(shallow(<EventPromo data={defaultData} />).is('.event-promo')).to.equal(true)
   })
 })
