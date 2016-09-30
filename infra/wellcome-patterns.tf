0f9458ffc5532181c80705fe0ddb32852c8fab2e
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-09-30 16:48:11 +0100

    terraform infra

--- infra/wellcome-patterns.tf
+++ infra/wellcome-patterns.tf
@@ -1,3 +1,5 @@
+variable "circle_user" {}
+
 provider "aws" {
   region     = "eu-west-1"
 }
@@ -11,3 +13,27 @@ resource "aws_s3_bucket" "wellcome_patterns" {
     index_document = "index.html"
   }
 }
+
+resource "aws_iam_user_policy" "website_bucket_write" {
+  name = "website-bucket-write"
+  user = "${aws_iam_user.circle_user.id}"
+  policy = "${file("write-bucket-policy.json")}"
+}
+
+resource "aws_iam_access_key" "circle_user" {
+  user = "${aws_iam_user.circle_user.name}"
+}
+
+resource "aws_iam_user" "circle_user" {
+  name = "${var.circle_user}"
+  force_destroy = true
+}
+
+output "circle_key" {
+  value = "${aws_iam_access_key.circle_user.id}"
+}
+
+output "circle_secret" {
+  value = "${aws_iam_access_key.circle_user.secret}"
+}
+
